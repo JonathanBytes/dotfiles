62157f4fe51e57d6403c0ae441f7e084c1b3da83
Jonathan ArchDell <jonathancardonaramirez@gmail.com>
2022-07-03 11:27:01 -0500

    .

--- Lvim/config.lua
+++ Lvim/config.lua
@@ -185,8 +185,12 @@ lvim.plugins = {
   },
   -- Easymotion
   {
-    'phaazon/hop.nvim',
-    branch = 'v1', -- optional but strongly recommended
+      'phaazon/hop.nvim',
+      branch = 'v1', -- optional but strongly recommended
+      config = function()
+        -- you can configure Hop the way you like here; see :h hop-config
+        require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+      end
   },
   {
     'neoclide/coc.nvim',
