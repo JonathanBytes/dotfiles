ad5bc7f8059fde109c5c65b7d893225b1f1cfc74
Jonathan ArchDell <jonathancardonaramirez@gmail.com>
2023-07-20 14:58:59 -0500

    Fix battery dir

--- .config/bspwm/polybar/scripts/battery-combined-tlp/change.sh
+++ .config/bspwm/polybar/scripts/battery-combined-tlp/change.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-state=$(cat /home/jonathan/polybar-scripts/polybar-scripts/battery-combined-tlp/var)
+state=$(cat /home/jonathan/.config/bspwm/polybar/scripts/battery-combined-tlp/var)
 
 if [ $state == 0 ]
 then
-  sed -i 's/0/1/g' /home/jonathan/polybar-scripts/polybar-scripts/battery-combined-tlp/var
+  sed -i 's/0/1/g' /home/jonathan/.config/bspwm/polybar/scripts/battery-combined-tlp/var
 else
-  sed -i 's/1/0/g' /home/jonathan/polybar-scripts/polybar-scripts/battery-combined-tlp/var
+  sed -i 's/1/0/g' /home/jonathan/.config/bspwm/polybar/scripts/battery-combined-tlp/var
 fi
