23f456d231cddf157d4749ea7f4ff9029cbea74e
Jonathan ArchDell <jonathancardonaramirez@gmail.com>
2023-07-31 17:02:44 -0500

    Commentary

--- .config/bspwm/polybar/scripts/toggletouchpad.sh
+++ .config/bspwm/polybar/scripts/toggletouchpad.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
+# Debian
 id_touchpad=$(xinput | grep -o 'Touchpad.*id=[0-9]\+' | grep -o '[0-9]\+')
 prop=$(xinput list-props "$id_touchpad" | grep -o 'Device Enabled (\([0-9]\+\))' | grep -o '[0-9]\+')
 state=$(xinput list-props "$id_touchpad" | grep -o 'Device Enabled.*: *[^ ]*$' | grep -o '[0-9]\+$')
+# ----- 
 
-# temp_state=$(synclient | grep 'TouchpadOff')
-
-# state=${temp_state: -1}
+# temp_state=$(synclient | grep 'TouchpadOff') # ArchLinux
+# state=${temp_state: -1} # ArchLinux
 
 if [ $state == 1 ]
 then
-  # synclient TouchpadOff=1
-  xinput set-prop $id_touchpad $prop 0
+  # synclient TouchpadOff=1 # ArchLinux
+  xinput set-prop $id_touchpad $prop 0 # Debian
 else
-  #label-foreground = ${color.RED}
-  # synclient TouchpadOff=0
-  xinput set-prop $id_touchpad $prop 1
+  # synclient TouchpadOff=0 # ArchLinux
+  xinput set-prop $id_touchpad $prop 1 # Debian
 fi
